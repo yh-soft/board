877dea9cf223b7beceff7a5887e43d4df3c9fd62
humit <jhjang1005@naver.com>
2024-07-21 10:12:10 +0900

    add UserEntity and UserRepository (#6)

---
+++ domain/src/main/java/com/yhsoft/board/domain/user/dao/UserRepository.java
@@ -0,0 +1,10 @@
+package com.yhsoft.board.domain.user.dao;
+
+import com.yhsoft.board.domain.user.domain.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByUsername(String username);
+}
---
+++ domain/src/main/java/com/yhsoft/board/domain/user/domain/UserStatus.java
@@ -0,0 +1,5 @@
+package com.yhsoft.board.domain.user.domain;
+
+public enum UserStatus {
+    REGISTER, VERIFY, LOCKED
+}
