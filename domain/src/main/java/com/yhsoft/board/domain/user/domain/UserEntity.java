8773dc184bf127fac40cd4f749706bbb46ebbc79
Joonho Jang <jhjang1005@naver.com>
2024-07-21 10:10:06 +0900

    add UserEntity and UserRepository

---
+++ domain/src/main/java/com/yhsoft/board/domain/user/domain/UserEntity.java
@@ -0,0 +1,34 @@
+package com.yhsoft.board.domain.user.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user_entity")
+public class UserEntity {
+    @Id
+    @GeneratedValue
+    private Long userId;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserStatus status;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+}
